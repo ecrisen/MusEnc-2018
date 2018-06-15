5d998d05358ee28c0a9cef5208726d4da357c0df
ecrisen <40278829+ecrisen@users.noreply.github.com>
2018-06-15 10:51:36 -0700

    updates

--- js/audio.js
+++ js/audio.js
@@ -2,7 +2,7 @@
 // Get performance data from MEI file
 /* Setup and send the request */
 var xhr = new XMLHttpRequest()
-xhr.open('GET', 'data/example2.mei') // <-- change this to the right file
+xhr.open('GET', 'data/Sie_liebten_sich_beide__Op._13_No._2_Clara_Schumann_nee_Wieck.xml') // <-- change this to the right file
 xhr.send(null)
 
 /* Deal with the response */
